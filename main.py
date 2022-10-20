3585605e2fcbc2ce5ec7fe1aad908d4a508d679d
Fostiriy <burakov.aa@students.dvfu.ru>
2022-10-20 10:28:20 +1000

    Заполнить таблицы

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Создание БД
+# Создание и заполнение БД
 
 import sqlite3
 
 # создаем базу данных и устанавливаем соединение с ней
 con = sqlite3.connect("poseidon.sqlite")
 
+# создание таблиц
 # открываем файл с дампом базой данных
 f_damp = open('db_creating.sql', 'r', encoding='utf-8-sig')
 # читаем данные из файла
@@ -17,8 +18,14 @@ con.executescript(damp)
 # сохраняем информацию в базе данных
 con.commit()
 
-# создаем курсор
-cursor = con.cursor()
+
+# заполнение таблиц
+f_damp = open('db_filling.sql', 'r', encoding='utf-8-sig')
+damp = f_damp.read()
+f_damp.close()
+
+con.executescript(damp)
+con.commit()
 
 # закрываем соединение с базой
 con.close()
